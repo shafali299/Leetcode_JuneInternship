acad899e48f8d6708bd21aac9799b94a25ba9479
shafali299 <99547134+shafali299@users.noreply.github.com>
2022-07-06 20:41:11 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        int first=0,second=1,ans=0;
+        if(n==0)
+            return 0;
+        else
+            if(n==1)
+            return 1;
+        for(int i=2;i<=n;i++)
+        {
+            ans=first+second;
+        
+            first=second;
+            second=ans;
+        }
+        return ans;
+        
+    }
+
+};
