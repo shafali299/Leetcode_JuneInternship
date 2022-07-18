9c651b6f46c905cba8b614915c1f2a9b1d5e50d1
shafali299 <99547134+shafali299@users.noreply.github.com>
2022-07-18 09:44:59 +0530

    Time: 301 ms (6.60%), Space: 19.3 MB (56.97%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) 
     {
         int n=nums.size();
-        for(int i=0;i<n-1;i++)
+        for(int i=0;i<n;i++)
         {
             if(nums[i]==0)
             {
